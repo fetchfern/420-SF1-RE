30e2f7fdf27a7573bf93b2d4e34ca9556708b9d2
François-Xavier Talbot <2240251@etu.cegepsth.qc.ca>
2024-09-18 10:27:39 -0400

    feat: command line template

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
+CMD_QUIT = 'quit'
+CMD_HELLO = 'hello'
+
 if __name__ == '__main__':
-    print('hello world')
+    while True:
+        try:
+            arguments = input('(): ').split()
+        except KeyboardInterrupt as _:
+            # pour gerer les interrupt comme ctrl-c proprement
+            break
+        else:
+            if len(arguments) == 0:
+                print('erreur: aucun arguments donne')
+                continue
+
+            commande = arguments[0]
+
+            if commande == CMD_QUIT:
+                break
+            elif commande == CMD_HELLO:
+                sujet = arguments[1] if len(arguments) >= 2 else 'world'
+                print(f'hello {sujet}')
+            else:
+                print(f'erreur: commande "{commande}" inconnue')
